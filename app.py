2d1553462caf07198d02cdc817a8ba984b7bb813
Kholikulov Elyor <152951460+Elyorcode@users.noreply.github.com>
2024-04-13 21:32:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from fastai.vision.all import *
 import pathlib
 import plotly.express as px
+import platform
 
-# pathlib.WindowsPath attributini to'g'ri ishlatish
-pathlib.PosixPath = pathlib.WindowsPath
-
+plt = platform.system()
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+    
 #title
 st.title('Transportni klassifikatsiya qiluvchi model')
 
